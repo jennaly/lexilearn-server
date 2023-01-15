f307b5bf1555174028eeff08449ce1228e7e7491
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-15 13:31:16 -0800

    add validation to password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,19 @@ const userSchema = new mongoose.Schema({
 
 // statics cannot be declared with arrow functions
 userSchema.statics.signup = async function (email, password) {
+
+    // validation 
+    if (!email || !password) {
+        throw Error('All fields must be filled');
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid');
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough');
+    }
 
     const userExists = await this.findOne({ email });;
 
